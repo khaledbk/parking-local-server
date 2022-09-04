bbf64f70920faa5e00096cdb6deda12f17552e19
SPRI <khaledbenk@gmail.com>
2022-09-03 23:14:26 -0400

    test

--- index.js
+++ index.js
@@ -4,20 +4,74 @@ import bodyParser from "body-parser";
 import { eventsHandler } from "./src/events/cameras/index.js";
 import cron from "node-cron";
 const PORT = 3002;
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const handleCamera = (req, res) => {
-  console.log(req);
 
+app.set("views",path.join(process.cwd(),"views"))
+
+// View Engine Setup
+app.set("view engine","ejs")
+    
+// var upload = multer({ dest: "Upload_folder_name" })
+// If you do not want to use diskStorage then uncomment it
+    
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+  
+        // Uploads is the Upload_folder_name
+        cb(null, "public")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname)
+    }
+  })
+       
+// Define the maximum size for uploading
+// picture i.e. 1 MB. it is optional
+const maxSize = 1 * 1000 * 10000;
+    
+var upload = multer({ 
+    storage: storage,
+    limits: { fileSize: maxSize }, 
+  
+// mypic is the name of file attribute
+}).any();     
+
+const handleDefault = (req, res) => {
+  console.log(JSON.stringify(req.headers));
   console.log("====================================");
-  res.send(200);
+
+    // Error MiddleWare for multer file upload, so if any
+    // error occurs, the image would not be uploaded!
+    upload(req,res,function(err) {
+  
+        if(err) {
+            console.log("multer err: ", err)
+            // ERROR occurred (here it can be occurred due
+            // to uploading image of size greater than
+            // 1MB or uploading different file type)
+            res.send(err)
+        }
+        else {
+            res.send(200); // res.write() instead of res.send()
+          
+            // If client closes connection, stop sending events
+            
+
+        }
+    }) 
 };
-app.get("/cameras", eventsHandler);
-app.get("/", handleCamera);
+
+app.get("/sse", eventsHandler);
+app.post("*", handleDefault);
 
 // cron.schedule("20 * * * * *", () => {
 //   console.log("running a task every 20 sec");
--- src/events/cameras/index.js
+++ src/events/cameras/index.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-
+import fs from "fs"
 export const eventsHandler = (req, res) => {
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Content-Type", "text/event-stream");
@@ -7,28 +7,21 @@ export const eventsHandler = (req, res) => {
   res.setHeader("Connection", "keep-alive");
   res.flushHeaders(); // flush the headers to establish SSE with client
 
-  let counter = 0;
-  let interValID = setInterval(() => {
-    counter++;
+  let data = ""
 
-    if (counter >= 100) {
-      clearInterval(interValID);
-      res.end(); // terminates SSE session
-      return;
-    }
-    let result = {
-      _id: uuidv4(),
-      image: "",
-      status: "UNREAD",
-      sentAt: new Date(),
-    };
-    res.write(`data: ${JSON.stringify(result)}\n\n`); // res.write() instead of res.send()
-  }, 15000);
+  console.log("multer success")
+  // SUCCESS, image successfully uploaded
+  data = fs.readFileSync("./public/event_data")
+  data = JSON.parse(data);
+
+  console.log('file readed ...')
+
+  res.write(`data: ${JSON.stringify({name:uuidv4()})}`); // res.write() instead of res.send()
 
   // If client closes connection, stop sending events
   res.on("close", () => {
     console.log("client dropped me");
-    clearInterval(interValID);
+    //clearInterval(interValID);
     res.end();
   });
 };
