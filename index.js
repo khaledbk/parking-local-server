55f3edebea2d5050d65b9c850ef6d89d5f6f8fc5
KhaledBK <khaledbenk@gmail.com>
2022-09-29 23:43:50 +0100

    feat: ading log and new logic

--- index.js
+++ index.js
@@ -2,12 +2,38 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { eventsHandler } from "./src/events/cameras/index.js";
-import cron from "node-cron";
 import multer from "multer";
 import moment from "moment";
+import fs from "fs";
+import path from "path";
 
 const PORT = 3002;
 export const app = express();
+const directory = "public";
+fs.readdir(directory, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(directory, file), (err) => {
+      if (err) throw err;
+    });
+  }
+});
+
+fs.writeFile(
+  "./log.txt",
+  `${moment()
+    .format("YYYY-MM-DD HH:mm:ss")
+    .toString()}: ========= server started ========= \n`,
+  { flag: "a+" },
+  (err) => {
+    if (err) {
+      console.error(err);
+    }
+    // file written successfully
+  }
+);
+app.use(bodyParser({ limit: "50mb" }));
 let test = 1;
 app.use(cors());
 app.use(bodyParser.json());
@@ -19,8 +45,8 @@ var storage = multer.diskStorage({
     cb(null, "public");
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + test + ".json");
-    cb(null, file.fieldname + ".json");
+    cb(null, test + "_" + file.originalname + ".json");
+    //cb(null, file.originalname);
   },
 });
 
@@ -35,30 +61,56 @@ var upload = multer({
 
 const handleCamera = (req, res) => {
   console.log(
-    "================================= POST-REQUEST ================================="
+    `================================= POST-REQUEST ${test}=================================`
   );
-  console.log("1 - ", JSON.stringify(req.headers));
+  fs.writeFile(
+    "./log.txt",
+    `${moment()
+      .format("YYYY-MM-DD HH:mm:ss")
+      .toString()}: ${test} POST REQUEST RECEIVED \n`,
+    { flag: "a+" },
+    (err) => {
+      if (err) {
+        console.error(err);
+      }
+      // file written successfully
+    }
+  );
 
   upload(req, res, function (err) {
-    console.log("2 - ", JSON.stringify(req.headers));
     if (err) {
       console.log("upload err: ", err);
       res.send(err);
     } else {
-      console.log(
-        moment().format("YYYY-MM-DD HH:mm:ss").toString(),
-        "==========",
-        test,
-        "=========="
-      );
       test++;
+      fs.writeFile(
+        "./log.txt",
+        `${moment().format("YYYY-MM-DD HH:mm:ss").toString()}: FILE WRITTEN \n`,
+        { flag: "a+" },
+        (err) => {
+          if (err) {
+            console.error(err);
+            fs.writeFile(
+              "./log.txt",
+              `${moment()
+                .format("YYYY-MM-DD HH:mm:ss")
+                .toString()}: [ERROR WRITE FILE] ${JSON.stringify(err)} \n`,
+              { flag: "a+" },
+              (err) => {
+                if (err) {
+                  console.error(err);
+                }
+                // file written successfully
+              }
+            );
+          }
+          // file written successfully
+        }
+      );
       //console.log("camera trigger");
       res.sendStatus(200);
     }
   });
-  console.log(
-    "================================= END ================================="
-  );
 };
 
 app.get("/cameras", eventsHandler);
--- src/events/cameras/index.js
+++ src/events/cameras/index.js
@@ -1,8 +1,22 @@
-import { v4 as uuidv4 } from "uuid";
 import fs from "fs";
 import moment from "moment";
-var eventNum = 1;
+import chokidar from "chokidar";
 
+let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = "0123456789";
+
+const carsImg = [
+  "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=",
+  "iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+];
+
+const generateImmat = () =>
+  characters.charAt(Math.floor(Math.random() * characters.length)) +
+  numbers.charAt(Math.floor(Math.random() * numbers.length)) +
+  characters.charAt(Math.floor(Math.random() * characters.length)) +
+  numbers.charAt(Math.floor(Math.random() * numbers.length)) +
+  characters.charAt(Math.floor(Math.random() * characters.length)) +
+  numbers.charAt(Math.floor(Math.random() * numbers.length));
 export const eventsHandler = (req, res) => {
   res.setHeader("Cache-Control", "no-store");
   res.setHeader("Content-Type", "text/event-stream");
@@ -10,30 +24,76 @@ export const eventsHandler = (req, res) => {
   res.setHeader("Connection", "keep-alive");
   res.flushHeaders(); // flush the headers to establish SSE with client
 
-  fs.watchFile("./public/event_data.json", function (current, previous) {
-    console.log("Event | ", moment().format("YYYY-MM-DD HH:mm:ss").toString());
-    let data = "";
-
-    data = fs.readFileSync("./public/event_data.json");
-    data = JSON.parse(data);
+  chokidar.watch("./public").on("all", (event, path) => {
     console.log(
-      "file read ...",
-      moment().format("YYYY-MM-DD HH:mm:ss").toString()
+      moment().format("YYYY-MM-DD HH:mm:ss").toString(),
+      "[Action]:",
+      event,
+      ` on this file =====>  ./${path}`
     );
+    fs.writeFile(
+      "./log.txt",
+      `${moment()
+        .format("YYYY-MM-DD HH:mm:ss")
+        .toString()}: FILE ACTION TRIGGERED \n`,
+      { flag: "a+" },
+      (err) => {
+        if (err) {
+          console.error(err);
+        }
+        // file written successfully
+      }
+    );
+    let data = "";
+    let result;
+    if (event === "add") {
+      try {
+        data = fs.readFileSync(`./${path}`);
+        data = JSON.parse(data);
+        // data.result.images.lp_img =
+        //   carsImg[Math.floor(Math.random() * carsImg.length)];
+        // data.result.images.normal_img =
+        //   carsImg[Math.floor(Math.random() * carsImg.length)];
+        // data.result.images.aux_img =
+        //   carsImg[Math.floor(Math.random() * carsImg.length)];
 
-    let result = {
-      status: "UNREAD",
-      sentAt: moment().format("YYYY-MM-DD HH:mm:ss").toString(),
-      data: data.result,
-    };
+        // data.result.anpr.text = generateImmat();
+        result = {
+          status: "UNREAD",
+          sentAt: moment().format("YYYY-MM-DD HH:mm:ss").toString(),
+          data: data.result,
+        };
+        if (res.writableEnded) return;
+        res.write(`data: ${JSON.stringify(result)}\n\n`);
+        fs.writeFile(
+          "./log.txt",
+          `${moment().format("YYYY-MM-DD HH:mm:ss").toString()}: EVENT SENT \n`,
+          { flag: "a+" },
+          (err) => {
+            if (err) {
+              console.error(err);
+            }
+            // file written successfully
+          }
+        );
+        console.log(
+          moment().format("YYYY-MM-DD HH:mm:ss").toString(),
+          "[Log]: event sent"
+        );
+      } catch (e) {
+        console.log("[ERROR]", e);
+      }
+    }
+
     // res.write() instead of res.send()
-    if (res.writableEnded) return;
-    res.write(`data: ${JSON.stringify(result)}\n\n`);
     console.log(
-      "Event Sent at | ",
-      moment().format("YYYY-MM-DD HH:mm:ss").toString()
+      "================================= END ================================="
     );
   });
+
+  // fs.watchFile("./public/event_data.json", function (current, previous) {
+
+  // });
 
   // If client closes connection, stop sending events
   res.on("close", () => {
