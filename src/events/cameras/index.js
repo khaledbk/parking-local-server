62fbe2b9c6ffd1fef9ecde675405e377af992280
KhaledBK <khaledbenk@gmail.com>
2022-09-29 02:56:41 +0100

    feat: add logs

--- src/events/cameras/index.js
+++ src/events/cameras/index.js
@@ -1,31 +1,33 @@
 import { v4 as uuidv4 } from "uuid";
 import fs from "fs";
 
+var eventNum = 1;
+
 export const eventsHandler = (req, res) => {
   res.setHeader("Cache-Control", "no-store");
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Connection", "keep-alive");
   res.flushHeaders(); // flush the headers to establish SSE with client
-  
-  fs.watchFile("./public/event_data.json",function(current, previous) {
-    console.log("File changed!");
-    let data = ""
 
-    data = fs.readFileSync("./public/event_data.json")
+  fs.watchFile("./public/event_data.json", function (current, previous) {
+    console.log("Event | ", new Date());
+    let data = "";
+
+    data = fs.readFileSync("./public/event_data.json");
     data = JSON.parse(data);
-    console.log('file read ...')
-    
+    console.log("file read ...");
+
     let result = {
       status: "UNREAD",
       sentAt: new Date(),
-      data: data.result
+      data: data.result,
     };
     // res.write() instead of res.send()
     if (res.writableEnded) return;
     res.write(`data: ${JSON.stringify(result)}\n\n`);
+    console.log("Event Sent at| ", new Date());
   });
-
 
   // If client closes connection, stop sending events
   res.on("close", () => {
