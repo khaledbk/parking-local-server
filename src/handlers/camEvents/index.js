10799597f3c85421159869cab9dda7eb15fa4e2d
Khaled BK <khaledbenk@gmail.com>
2023-04-15 17:22:40 -0400

    fix: delete logs

--- src/handlers/camEvents/index.js
+++ src/handlers/camEvents/index.js
@@ -34,19 +34,19 @@ export const camEventsHandler = (req, res) => {
   console.log(
     `================================= CAM REQUEST DETECTED =================================`
   );
-  fs.writeFile(
-    "./log.txt",
-    `${moment()
-      .format("YYYY-MM-DD HH:mm:ss")
-      .toString()}: ${scans} POST REQUEST RECEIVED \n`,
-    { flag: "a+" },
-    (err) => {
-      if (err) {
-        console.error(err);
-      }
-      // file written successfully
-    }
-  );
+  // fs.writeFile(
+  //   "./log.txt",
+  //   `${moment()
+  //     .format("YYYY-MM-DD HH:mm:ss")
+  //     .toString()}: ${scans} POST REQUEST RECEIVED \n`,
+  //   { flag: "a+" },
+  //   (err) => {
+  //     if (err) {
+  //       console.error(err);
+  //     }
+  //     // file written successfully
+  //   }
+  // );
 
   upload(req, res, function (err) {
     if (err) {
@@ -54,30 +54,30 @@ export const camEventsHandler = (req, res) => {
       res.send(err);
     } else {
       scans++;
-      fs.writeFile(
-        "./log.txt",
-        `${moment().format("YYYY-MM-DD HH:mm:ss").toString()}: FILE WRITTEN \n`,
-        { flag: "a+" },
-        (err) => {
-          if (err) {
-            console.error(err);
-            fs.writeFile(
-              "./log.txt",
-              `${moment()
-                .format("YYYY-MM-DD HH:mm:ss")
-                .toString()}: [ERROR WRITE FILE] ${JSON.stringify(err)} \n`,
-              { flag: "a+" },
-              (err) => {
-                if (err) {
-                  console.error(err);
-                }
-                // file written successfully
-              }
-            );
-          }
-          // file written successfully
-        }
-      );
+      // fs.writeFile(
+      //   "./log.txt",
+      //   `${moment().format("YYYY-MM-DD HH:mm:ss").toString()}: FILE WRITTEN \n`,
+      //   { flag: "a+" },
+      //   (err) => {
+      //     if (err) {
+      //       console.error(err);
+      //       fs.writeFile(
+      //         "./log.txt",
+      //         `${moment()
+      //           .format("YYYY-MM-DD HH:mm:ss")
+      //           .toString()}: [ERROR WRITE FILE] ${JSON.stringify(err)} \n`,
+      //         { flag: "a+" },
+      //         (err) => {
+      //           if (err) {
+      //             console.error(err);
+      //           }
+      //           // file written successfully
+      //         }
+      //       );
+      //     }
+      //     // file written successfully
+      //   }
+      // );
       //console.log("camera trigger");
       res.sendStatus(200);
     }
